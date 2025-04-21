648eb1fefdfa1b173ce1e02ac35616d751ee97ad
Srinivas Kandukuri <ksrinivas.cse@gmail.com>
2025-04-21 19:23:46 +0530

    stack && array

--- problems/neetcode-blind-75/TwoPointer/ContainerOfWater.go
+++ problems/neetcode-blind-75/TwoPointer/ContainerOfWater.go
@@ -1,1 +1,59 @@
-package TwoPointer
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
+	fmt.Println(maxArea(arr))
+}
+
+// brute force
+func containerOfWater(heights []int) int {
+	maxArea := 0
+	for i := 0; i < len(heights); i++ {
+
+		for j := i + 1; j < len(heights); j++ {
+			h := min(heights[i], heights[j])
+			w := j - i
+			area := h * w
+			if area > maxArea {
+				maxArea = area
+			}
+		}
+	}
+	return maxArea
+}
+
+func maxArea(heights []int) int {
+
+	left := 0
+	right := len(heights) - 1
+
+	maxArea := 0
+
+	for left < right {
+		h := min(heights[left], heights[right])
+		w := right - left
+
+		area := h * w
+		if area > maxArea {
+			maxArea = area
+		}
+
+		if heights[left] < heights[right] {
+			left++
+		} else {
+			right--
+		}
+
+	}
+	return maxArea
+}
+
+func min(a, b int) int {
+
+	if a < b {
+		return a
+	}
+	return b
+}
--- problems/neetcode-blind-75/stack/validParentheses.go
+++ problems/neetcode-blind-75/stack/validParentheses.go
@@ -1,1 +1,32 @@
-package stack
+package main
+
+import "fmt"
+
+func main() {
+	arr := "()[]{}"
+	fmt.Println(validParentheses(arr))
+}
+
+func validParentheses(s string) bool {
+
+	var stack []rune
+
+	mapping := map[rune]rune{
+		'}': '{',
+		']': '[',
+		')': '(',
+	}
+
+	for _, ch := range s {
+		if open, exist := mapping[ch]; exist {
+			if len(stack) == 0 || stack[len(stack)-1] != open {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, ch)
+		}
+	}
+
+	return len(stack) == 0
+}
